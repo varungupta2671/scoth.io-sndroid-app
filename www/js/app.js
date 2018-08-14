fcc826e326e6989b9f31c8d38808958bf448d0ba
Varun Gupta <varungupta2671@gmail.com>
2018-08-15 02:54:48 +0530

    ui changes

--- www/js/app.js
+++ www/js/app.js
@@ -1,14 +1,113 @@
-'use strict';
 
-angular.module('myApp', [
-    'ngTouch',
-    'ngRoute',
-    'ngAnimate',
-    'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/employees', { templateUrl: 'partials/view.html', controller: 'EmployeeListCtrl' });
-    $routeProvider.when('/employees/:employeeId', { templateUrl: 'partials/view-1.html', controller: 'EmployeeDetailCtrl' });
-    $routeProvider.when('/employees/:employeeId/reports', { templateUrl: 'partials/view-2.html', controller: 'ReportListCtrl' });
-    $routeProvider.otherwise({ redirectTo: '/employees' });
+var app = angular.module('myApp', ['onsen']);
+
+// Run
+
+app.run(function () {
+  var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+});
+
+// Config
+
+app.config( function ($httpProvider) {
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+});
+
+// Service
+
+app.service('VideosService', ['$window', '$rootScope', '$log', function ($window, $rootScope, $log) {
+
+  var service = this;
+
+  var youtube = {
+    ready: false,
+    player: null,
+    playerId: null,
+    videoId: null,
+    videoTitle: null,
+    playerHeight: '100%',
+    playerWidth: '100%',
+    state: 'stopped'
+  };
+  var results = [];
+  var history = [];
+
+  $window.onYouTubeIframeAPIReady = function () {
+    $log.info('Youtube API is ready');
+    youtube.ready = true;
+    service.bindPlayer('placeholder');
+    service.loadPlayer();
+    $rootScope.$apply();
+  };
+
+  this.bindPlayer = function (elementId) {
+    $log.info('Binding to ' + elementId);
+    youtube.playerId = elementId;
+  };
+
+  this.createPlayer = function () {
+    $log.info('Creating a new Youtube player for DOM id ' + youtube.playerId + ' and video ' + youtube.videoId);
+    return new YT.Player(youtube.playerId, {
+      height: youtube.playerHeight,
+      width: youtube.playerWidth,
+      playerVars: {
+        rel: 0,
+        showinfo: 0
+      }
+    });
+  };
+
+  this.loadPlayer = function () {
+    if (youtube.ready && youtube.playerId) {
+      if (youtube.player) {
+        youtube.player.destroy();
+      }
+      youtube.player = service.createPlayer();
+    }
+  };
+
+  this.launchPlayer = function (id, title) {
+    youtube.player.loadVideoById(id);
+    youtube.videoId = id;
+    youtube.videoTitle = title;
+    return youtube;
+  }
+
+  this.listResults = function (data, append) {
+    if (!append) {
+      results.length = 0;
+    }
+    for (var i = data.items.length - 1; i >= 0; i--) {
+      results.push({
+        id: data.items[i].id.videoId,
+        title: data.items[i].snippet.title,
+        description: data.items[i].snippet.description,
+        thumbnail: data.items[i].snippet.thumbnails.default.url,
+        author: data.items[i].snippet.channelTitle
+      });
+    }
+    return results;
+  }
+
+  this.archiveVideo = function (video) {
+    history.unshift(video);
+    return history;
+  };
+
+  this.getYoutube = function () {
+    return youtube;
+  };
+
+  this.getResults = function () {
+    return results;
+  };
+
+  this.getHistory = function () {
+    return history;
+  };
+
 }]);
+
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,23 +1,79 @@
-'use strict';
 
-angular.module('myApp.controllers', [])
-    .controller('MainCtrl', ['$scope', '$rootScope', '$window', '$location', function ($scope, $rootScope, $window, $location) {
-        $scope.slide = '';
-        $rootScope.back = function() {
-          $scope.slide = 'slide-right';
-          $window.history.back();
+angular.module('myApp')
+
+// Controller
+
+.controller('VideosController', function ($scope, $http, $log, VideosService) {
+
+    init();
+
+    function init() {
+      $scope.youtube = VideosService.getYoutube();
+      $scope.results = VideosService.getResults();
+      $scope.history = VideosService.getHistory();
+    }
+
+    $scope.isAndroid = function() {
+      return ons.platform.isAndroid();
+    }
+
+    $scope.toolbarTitle = 'Search';
+    $scope.updateToolbar = function(title) {
+      $scope.toolbarTitle = title;
+    }
+
+    $scope.focusInput = function(platform) {
+      document.getElementById(platform + '-search-input').focus();
+    };
+
+    $scope.blurInput = function(platform) {
+      document.getElementById(platform + '-search-input').blur();
+    };
+
+    $scope.launch = function (video, archive) {
+      VideosService.launchPlayer(video.id, video.title);
+      if (archive) {
+      	VideosService.archiveVideo(video);
+      }
+      $log.info('Launched id:' + video.id + ' and title:' + video.title);
+    };
+
+    $scope.nextPageToken = '';
+    $scope.labelSearch = 'You haven\'t searched for any video yet!';
+    $scope.labelHistory = 'You haven\'t watched any video yet!';
+
+    $scope.loading = false;
+
+    $scope.loadMore = function(done) {
+      $scope.search(false).then(done);
+    };
+
+    $scope.search = function (isNewQuery) {
+      $scope.loading = true;
+      return $http.get('https://www.googleapis.com/youtube/v3/search', {
+        params: {
+          key: 'AIzaSyAaVxe2e6AbU3FD2pKTQh1_AySRHC1NY8I',
+          type: 'video',
+          maxResults: '10',
+          pageToken: isNewQuery ? '' : $scope.nextPageToken,
+          part: 'id,snippet',
+          fields: 'items/id,items/snippet/title,items/snippet/description,items/snippet/thumbnails/default,items/snippet/channelTitle,nextPageToken',
+          q: this.query
         }
-        $rootScope.go = function(path){
-          $scope.slide = 'slide-left';
-          $location.url(path);
+      })
+      .success( function (data) {
+        if (data.items.length === 0) {
+          $scope.label = 'No results were found!';
         }
-    }])
-    .controller('EmployeeListCtrl', ['$scope', 'Employee', function ($scope, Employee) {
-        $scope.employees = Employee.query();
-    }])
-    .controller('EmployeeDetailCtrl', ['$scope', '$routeParams', 'Employee', function ($scope, $routeParams, Employee) {
-        $scope.employee = Employee.get({employeeId: $routeParams.employeeId});
-    }])
-    .controller('ReportListCtrl', ['$scope', '$routeParams', 'Report', function ($scope, $routeParams, Report) {
-        $scope.employees = Report.query({employeeId: $routeParams.employeeId});
-    }]);
+        VideosService.listResults(data, $scope.nextPageToken && !isNewQuery);
+        $scope.nextPageToken = data.nextPageToken;
+        $log.info(data);
+        $scope.loading = false;
+      })
+      .error( function (e) {
+        $log.info('Search error: ', e);
+        $scope.loading = false;
+      })
+      ;
+    };
+});
