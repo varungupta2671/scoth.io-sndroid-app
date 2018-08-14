6996fac3cdc3862a0d23bb033f2af023d816ca42
Varun Gupta <varungupta2671@gmail.com>
2018-08-15 02:48:53 +0530

    UI changes

--- www/js/app.js
+++ www/js/app.js
@@ -4,12 +4,11 @@ angular.module('myApp', [
     'ngTouch',
     'ngRoute',
     'ngAnimate',
-    'myApp.controllers',
-    'myApp.memoryServices'
+    'myApp.controllers'
 ]).
-config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/employees', {templateUrl: 'partials/employee-list.html', controller: 'EmployeeListCtrl'});
-    $routeProvider.when('/employees/:employeeId', {templateUrl: 'partials/employee-detail.html', controller: 'EmployeeDetailCtrl'});
-    $routeProvider.when('/employees/:employeeId/reports', {templateUrl: 'partials/report-list.html', controller: 'ReportListCtrl'});
-    $routeProvider.otherwise({redirectTo: '/employees'});
+config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/employees', { templateUrl: 'partials/view.html', controller: 'EmployeeListCtrl' });
+    $routeProvider.when('/employees/:employeeId', { templateUrl: 'partials/view-1.html', controller: 'EmployeeDetailCtrl' });
+    $routeProvider.when('/employees/:employeeId/reports', { templateUrl: 'partials/view-2.html', controller: 'ReportListCtrl' });
+    $routeProvider.otherwise({ redirectTo: '/employees' });
 }]);
