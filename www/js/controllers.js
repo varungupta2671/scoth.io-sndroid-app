9143a2c55ca92ec56110e24cc5ee1b7aa65ceeeb
Varun Gupta <varungupta2671@gmail.com>
2018-08-15 11:30:42 +0530

    ui changes

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -27,4 +27,15 @@ angular.module('myApp')
         $scope.search(false).then(done);
     };
 
+})
+
+.controller('PageController', function($timeout) {
+    this.items = [3, 2, 1];
+
+    this.load = function($done) {
+        $timeout(function() {
+            this.items.unshift(this.items.length + 1);
+            $done();
+        }.bind(this), 1000);
+    }.bind(this);
 });
